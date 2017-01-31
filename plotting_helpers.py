239d92ae154816f8744b6b495ccc9bdd33d43505
Colin Morris <colin.morris2@gmail.com>
2017-01-31 15:43:00 -0500

    notebook tweaks

--- plotting_helpers.py
+++ plotting_helpers.py
@@ -1,7 +1,25 @@
+import math
+
+def _percentify(tick):
+    # I know this is an abomination and there's probably an elegant way to
+    # do it with proper use of str.format.
+    tick *= 100
+    dp = 1 # decimal points
+    epsilon = 10**-3
+    if tick == 0:
+        return '0%'
+    elif abs(tick % 1 - .5) < epsilon:
+        dp = 1
+    elif 0 < tick < 1:
+        dp = int(abs(math.log(tick, 10)-1))
+    elif tick % 1 < epsilon or (1 - tick % 1 < epsilon):
+        dp = 0
+    return ('{:.'+str(dp)+'f}%').format(tick)
+
 def xlabel_pct(ax):
-    ax.set_xticklabels('{}%'.format(x*100) for x in ax.get_xticks())
+    ax.set_xticklabels(map(_percentify, ax.get_xticks()))
 def ylabel_pct(ax):
-    ax.set_yticklabels('{}%'.format(y*100) for y in ax.get_yticks())
+    ax.set_yticklabels(map(_percentify, ax.get_yticks()))
     
 def plot_percent(df, horizontal=False, **kwargs):
     pfunc = df.plot.barh if horizontal else df.plot.bar
