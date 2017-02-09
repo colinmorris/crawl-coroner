d64ff4a28eafb31e7ea6f9d3ecf192324a108b84
Colin Morris <colin.morris2@gmail.com>
2017-02-09 21:45:54 +0000

    notebook doodling

--- winrate.py
+++ winrate.py
@@ -7,5 +7,5 @@ def smoothed_winrate(df, C=10):
     def _smooth_wincol(won_col):
         return ( (won_col.sum() + (C * wr_prior))
                 / (C + len(won_col)) )
-    return df.groupby('pid')['won'].agg(_smooth_wincol).loc[df['pid']]
+    return df.groupby('pid')['won'].agg(_smooth_wincol).loc[df['pid']].values
 
